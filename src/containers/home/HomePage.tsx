2a30fcb263ebb91cfbfbcd93bab66a73ad1b4ce9
yashimisra <Yashi_Misra@Mckinsey.com>
2023-04-29 13:52:11 +0530

    (feat): homepage container.

---
+++ src/containers/home/HomePage.tsx
@@ -0,0 +1,19 @@
+import { Footer } from 'components/footer/Footer';
+import { Header } from 'components/header/Header';
+import { QuestionsGrid } from 'components/questionsGrid/QuestionsGrid';
+import React from 'react';
+
+export const HomePage = () => {
+  return (
+    <>
+      <div className="bg-graybg min-h-screen">
+        <Header />
+        <div className="mt-[52px]">
+          <QuestionsGrid />
+        </div>
+
+        <Footer />
+      </div>
+    </>
+  );
+};
