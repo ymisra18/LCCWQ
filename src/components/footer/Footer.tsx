1a159507919fa4d8d89c97adb754de8c4f9bf091
ymisra18 <ymisra18@gmail.com>
2023-05-24 13:16:48 +0530

    (fix):header, footer fix.

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 export const Footer = () => (
   <div
-    className="w-full h-16 flow-root font-roboto bg-black justify-between items-center p-2 px-5 z-20 cursor-default"
+    className="w-full h-16  font-roboto flex justify-center items-center p-2 px-5 z-20 cursor-default"
     data-testid="Footer"
   >
-    <p className="text-white text[20px] float-right mt-2">
-      Copyright © 2023 LCCWQ inc.
+    <p className="text-white text[20px] flex mt-2 pr-1">
+      Made with love by Yashi Misra 🤍
     </p>
   </div>
 );
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,13 +4,13 @@ export const Header = () => {
   return (
     <div
       data-testid="header"
-      className="h-16 font-robot flex justify-between p-2 px-5 items-center w-full z-20 bg-black"
+      className="h-14 font-robot flex justify-between p-2 px-5 items-center w-full z-20 bg-tableRowEven "
     >
       <div className="flex items-center">
         <img className=" h-[45px]" src={logo} alt="LCCWQ logo" />
-        <div className="absolute left-[4%] w-0.5 h-[45px] bg-white"></div>
-        <p className="font-roboto text-white text-[30px] ml-[35px] font-bold">
-          Leetcode company wise questions
+        <div className="absolute left-[4%] w-0.5 h-[35px] bg-white"></div>
+        <p className=" text-headerText text-[30px] ">
+          Leetcode Company Wise Questions
         </p>
       </div>
     </div>
