dec403da487943d877b0578a3615e06780a4223f
ymisra18 <ymisra18@gmail.com>
2023-05-24 14:03:24 +0530

    (fix): footer height.

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 export const Footer = () => (
   <div
-    className="w-full h-16  font-roboto flex justify-center items-center p-2 px-5 z-20 cursor-default"
+    className="w-full h-10 font-roboto flex justify-center items-center p-2 px-5 z-20 cursor-default"
     data-testid="Footer"
   >
     <p className="text-white text[20px] flex mt-2 pr-1">
