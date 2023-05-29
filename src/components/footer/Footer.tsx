7a38a01e9dab0ad9f50868db0779aa05b539ab90
ymisra18 <ymisra18@gmail.com>
2023-05-29 21:50:19 +0530

    (feat): add links to footer.

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -5,23 +5,33 @@ export const Footer = () => (
     data-testid="Footer"
   >
     <p className="text-white text[20px] flex mt-2 pr-1">
-      Made with love by Yashi Misra 🤍 |
+      © Made with love by Yashi Misra 🤍 |
     </p>
     <a
-      className="text-white text[20px] flex mt-2 pr-1"
+      className="text-white text[20px] flex mt-2 pr-1 hover:text-blue"
       href="https://github.com/ymisra18/LCCWQ/issues/new/choose"
       target="_blank"
       rel="noreferrer"
     >
-      Report Issues |
+      Report Issues
     </a>
+    <p className="text-white text[20px] flex mt-2 pr-1">|</p>
     <a
-      className="text-white text[20px] flex mt-2 pr-1"
+      className="text-white text[20px] flex mt-2 pr-1 hover:text-blue"
       href="https://github.com/ymisra18/LCCWQ"
       target="_blank"
       rel="noreferrer"
     >
       GitHub
+    </a>
+    <p className="text-white text[20px] flex mt-2 pr-1">|</p>
+    <a
+      className="text-white text[20px] flex mt-2 pr-1 hover:text-blue"
+      href="https://forms.gle/AizN731Q2wStQ6J8A"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Feedback
     </a>
   </div>
 );
