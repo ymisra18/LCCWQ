dcb4cc0d83a5c0d0c27f50aba51e3132ef9dd8e4
ymisra18 <ymisra18@gmail.com>
2023-05-29 21:31:35 +0530

    (feat): update logo and favicon.

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -7,8 +7,7 @@ export const Header = () => {
       className="h-14 font-robot flex justify-between p-2 px-5 items-center w-full z-20 bg-tableRowEven "
     >
       <div className="flex items-center">
-        {/* <img className=" h-[45px]" src={logo} alt="LCCWQ logo" /> */}
-        {/* <div className="absolute left-[4%] w-0.5 h-[35px] bg-white"></div> */}
+        <img className=" h-[45px]" src={logo} alt="LCCWQ logo" />
         <p className=" text-headerText text-[30px] ">
           Leetcode Company Wise Questions
         </p>
